dc0dd33bd9c5f88b174dafeaa0bdf8a3eefe726b
Rogero0o <402435491@qq.com>
2016-07-15 14:49:22 +0800

    Add LiveWallPaper moudle

---
+++ LiveWallPaper/src/main/java/com/roger/livewallpaper/data/VertexArray.java
@@ -0,0 +1,35 @@
+package com.roger.livewallpaper.data;
+
+import android.opengl.GLES20;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+/**
+ * Created by Administrator on 2016/7/5.
+ */
+public class VertexArray {
+  private final FloatBuffer floatBuffer;
+
+  public VertexArray(float[] vertexData) {
+    floatBuffer = ByteBuffer.allocateDirect(vertexData.length * Constands.BYTES_PER_FLOAT)
+        .order(ByteOrder.nativeOrder())
+        .asFloatBuffer()
+        .put(vertexData);
+  }
+
+  public void setVertexAttribPointer(int dataOffset, int attributeLocation, int componentCount,
+      int stride) {
+    floatBuffer.position(dataOffset);
+    GLES20.glVertexAttribPointer(attributeLocation, componentCount, GLES20.GL_FLOAT, false, stride,
+        floatBuffer);
+    GLES20.glEnableVertexAttribArray(attributeLocation);
+    floatBuffer.position(0);
+  }
+
+  public void updateBuffer(float[] vertexData, int start, int count) {
+    floatBuffer.position(start);
+    floatBuffer.put(vertexData, start, count);
+    floatBuffer.position(0);
+  }
+}
